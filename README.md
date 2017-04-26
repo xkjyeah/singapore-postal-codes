35344542e70d85e5af3d4e539baf0d275d3000dc
Daniel Sim <daniel.ssq89@gmail.com>
2017-04-26 23:57:22 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Singapore Postal Codes
+======================
+
+This is a dump of all Singapore postal codes retrieved from Onemap.sg.
+The download script and the data dump are attached.
+
+If you re-run the script and notice changes to the data dump, congratulations!
+You have spotted a change in the postal codes! Submit a pull request and I'll
+update the dump.
+
+Note: Use of the data is governed by the [Open Data Licence](https://www.onemap.sg/legal/opendatalicence.html)
+This data dump contains information from Onemap.sg postal code search accessed on 25 Apr 2017.
+
---
+++ download_postal_codes.py
@@ -0,0 +1,46 @@
+import requests
+import time
+from multiprocessing import Pool
+
+def pcode_to_data(pcode):
+    if int(pcode) % 1000 == 0:
+        print(pcode)
+    
+    page = 1
+    results = []
+    
+    while True:
+        try:
+            response = requests.get('http://developers.onemap.sg/commonapi/search?searchVal={0}&returnGeom=Y&getAddrDetails=Y&pageNum={1}'
+                                    .format(pcode, page)) \
+                               .json()
+        except requests.exceptions.ConnectionError as e:
+            print('Fetching {} failed. Retrying in 2 sec'.format(pcode))
+            
+            time.sleep(2)
+            continue
+            
+        results = results + response['results']
+    
+        if response['totalNumPages'] > page:
+            page = page + 1
+        else:
+            break
+            
+    return results
+
+import json
+
+if __name__ == '__main__':
+    pool = Pool(processes=5)
+    
+    postal_codes = range(0, 1000000)
+    postal_codes = ['{0:06d}'.format(p) for p in postal_codes]
+
+    all_buildings = pool.map(pcode_to_data, postal_codes)
+    
+    jstr = json.dumps([y for x in all_buildings for y in x], indent=2)
+
+    with open('buildings.json', 'w') as f:
+        f.write(jstr.encode('utf-8'))
+
